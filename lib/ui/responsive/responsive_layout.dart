91ef3cdd8201ca5004d9a7eddb4253ad7004c5e6
HongHac <144915081+chuonghoai@users.noreply.github.com>
2026-07-29 17:38:59 +0700

    feat: build core

---
+++ lib/ui/responsive/responsive_layout.dart
@@ -0,0 +1,21 @@
+// import 'package:flutter/material.dart';
+// class ResponsiveLayout extends StatelessWidget {
+//   final LocationRepository repository;
+
+//   const ResponsiveLayout({super.key});
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return LayoutBuilder(
+//       builder: (context, constraints) {
+//         if (constraints.maxWidth > 800) {
+//           return WebHomeScreen(repository: repository);
+//         } else {
+//           return MobileHomeScreen(repository: repository);
+//         }
+//       },
+//     );
+//   }
+// }
+
+class ResponsiveLayout {}
